ecaa8b8e33f856fde041a2bbee52e95672218523
Laura Archambault <laarchambault@gmail.com>
2019-11-01 17:13:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,18 +12,13 @@ def map(source_array)
 end
 
 
- def reduce(source_array, starting_value = nil)
-   if starting_value
-     reduce_tot = starting_value
-   else
-     reduce_tot = source_array[0] && starting_value
-   end
- 
+ def reduce(source_array, starting_value = 0)
+
 i = 0
   while i < source_array.length do
-    reduce_tot = yield(reduce_tot, source_array[i])
+    starting_value = yield(starting_value, source_array[i])
    # binding.pry
     i += 1
   end
-reduce_tot
+starting_value
 end
