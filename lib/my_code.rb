6f1a3ee62d58ec848a1df24086b29b4887c6718f
Laura Archambault <laarchambault@gmail.com>
2019-11-01 16:59:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,10 +11,15 @@ end
 
 
  def reduce(source_array, starting_value = 0)
+ if starting_value != 0 
+   reduce_tot = nil
+ else
+   reduce_tot = starting_value
+ end
  
 i = 0
   while i < source_array.length do
-    starting_value = yield(starting_value, source_array[i])
+    reduce_tot = yield(reduce_tot, source_array[i])
     i += 1
   end
 starting_value
